5724de06523ad9b29243fe6bd23d337b982fa019
pradeep007nc <pradeepnaidu2486@gmail.com>
2023-12-24 16:40:32 +0530

    minor update bookings

--- src/main/java/dev/pradeep/OnTravelsAssignment/Entity/Booking.java
+++ src/main/java/dev/pradeep/OnTravelsAssignment/Entity/Booking.java
@@ -2,6 +2,10 @@ package dev.pradeep.OnTravelsAssignment.Entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import dev.pradeep.OnTravelsAssignment.Dto.LocationDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,6 +13,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "bookings")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Booking {
 
     @Id
